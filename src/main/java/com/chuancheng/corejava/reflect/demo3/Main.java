a069b4cd9358df3243e0007ef3d32f688b122724
maochengcheng <984968817@qq.com>
2020-05-06 09:27:37 +0800

    测试

--- src/main/java/com/chuancheng/corejava/reflect/demo3/Main.java
+++ src/main/java/com/chuancheng/corejava/reflect/demo3/Main.java
@@ -2,18 +2,41 @@ package com.chuancheng.corejava.reflect.demo3;
 
 public class Main {
     public static void main(String[] args) {
-
+        basicOper();
     }
 
-    public void basicOper(){
-
+    public static void basicOper()  {
         /**
          * 获取一个Class的对象实例
          */
         Class clazz = Boy.class;
         Class clazz2 = new Boy().getClass();
-       // Class clazz3 = Class.forName()
+        try{
+            Class clazz3 = Class.forName("com.chuancheng.corejava.reflect.demo3.Boy");
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
 
+        try {
+            Class class4 = Main.class.getClassLoader().loadClass("com.chuancheng.corejava.reflect.demo3.Boy");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
+        int modifers = clazz.getModifiers();
+        System.out.println("modifers:"+modifers);
+        Package boyPkg = clazz.getPackage();
+        System.out.println("boyPkg:"+boyPkg);
+
+        System.out.println(clazz.getName());
+        System.out.println(clazz.getSimpleName());
+
+        System.out.println(clazz.getClassLoader());
+
+        System.out.println(clazz.getInterfaces().length);
+
+        System.out.println(clazz.getSuperclass());
+
+        System.out.println(clazz.getAnnotations().length);
     }
 }
