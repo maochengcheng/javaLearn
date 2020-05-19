cb7f0171f8bbcf34342df82838e04b5344bed7de
maochengcheng <984968817@qq.com>
2020-05-19 09:04:35 +0800

    集合

---
+++ src/main/java/com/chuancheng/corejava/collectionedu/Test.java
@@ -0,0 +1,12 @@
+package com.chuancheng.corejava.collectionedu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author maochengcheng
+ * @date 2020/5/18 0018
+ */
+public class Test {
+    List list = new ArrayList();
+}
