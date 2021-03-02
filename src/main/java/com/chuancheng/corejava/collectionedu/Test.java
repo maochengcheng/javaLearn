85bc8c749bafcbe47101745bc0f5a6c484f6fb15
maochengcheng <984968817@qq.com>
2021-03-02 21:22:28 +0800

    测试方法

--- src/main/java/com/chuancheng/corejava/collectionedu/Test.java
+++ src/main/java/com/chuancheng/corejava/collectionedu/Test.java
@@ -1,6 +1,7 @@
 package com.chuancheng.corejava.collectionedu;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -9,4 +10,14 @@ import java.util.List;
  */
 public class Test {
     List list = new ArrayList();
+
+    public static void main(String args[]){
+        Calendar calendar = Calendar.getInstance();
+        long systime = calendar.getTimeInMillis();
+        System.out.println(systime);
+        long dead = 31535000000l+systime;
+        System.out.println(dead);
+    }
+
+
 }
